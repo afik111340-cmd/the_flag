241d8780dc3eade95da452fc6bc12d65ae84803a
ianadivane2006@gmail.com <ianadivane2006@gmail.com>
2025-08-21 10:43:32 +0300

    my commit

---
+++ soldier.py
@@ -0,0 +1,8 @@
+"""
+Player character detection module.
+Will contain (among other things):
+.1. Player image.
+.2. Player position relative to its upper left corner.
+.3. Methods for calculating the position of the character's legs and body (to check for contact with a mine/flag).
+.4. Checking that the character does not go beyond the boundaries of the game window.
+"""
